056471b16a520ed6231dc9f7bc07ce1c37647787
BsWasKilled <bsbplays2006@gmail.com>
2026-09-09 10:45:00 -0300

    refactor: clean01 - corrige comentario de debitarCreditos que dizia 'adiciona' quando o metodo subtrai

--- src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -30,7 +30,7 @@ public class Usuario {
     }
 
     public void debitarCreditos(double valor) {
-        // adiciona o valor aos créditos do usuário
+        // subtrai do valor dos créditos do usuário
         this.creditos = this.creditos - valor;
     }
 
