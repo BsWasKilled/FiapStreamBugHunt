5dd50594c3d0bb5843dd5a5dcc143579a9739865
jvsen211 <jvsenmaneiro16@gmail.com>
2026-09-10 00:22:13 -0300

    fix: bug07 - documentario nao sobrescrevia calcularPrecoAluguel e cobrava 9.90 em vez de ser gratuito

--- src/main/java/br/com/fiap/streamfiap/model/Documentario.java
+++ src/main/java/br/com/fiap/streamfiap/model/Documentario.java
@@ -15,11 +15,6 @@ public class Documentario extends Conteudo {
         this.tema = tema;
     }
 
-    @Override
-    public double calcularPrecoAluguel() {
-        return 0.0;
-    }
-
     public String getTema() { return tema; }
     public void setTema(String tema) { this.tema = tema; }
 }
